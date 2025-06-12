4269db9c170b20d68cd3dcd749109390580fa84a
MoonOwlll <germogneov597@gmail.com>
2025-06-13 01:25:55 +0900

    Initial backend commit

--- models/db.js
+++ models/db.js
@@ -7,11 +7,12 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('✅ Connected to MySQL');
-});
+// db.connect((err) => {
+//   if (err) throw err;
+//   console.log('✅ Connected to MySQL');
+// });
 
 module.exports = db;
